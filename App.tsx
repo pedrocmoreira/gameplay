0cd25ce8ff8fcc3aa63519742d4ba8e4f7e807e6
Pedro Moreira <pedrocmoreir97@gmail.com>
2021-06-23 08:27:50 -0300

    Stating home screen

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@ import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
 import AppLoading from 'expo-app-loading';
 
-import { SignIn } from './src/screens/SignIn';
+import { Home } from './src/screens/Home';
 import { Background } from './src/components/Background';
 
 export default function App() {
@@ -28,7 +28,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      <SignIn />
+      <Home />
     </Background>
   )
 }
