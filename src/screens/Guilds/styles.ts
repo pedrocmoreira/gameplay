9ecc42e8362465122f2d8041f4a0466b18e2458e
Pedro Moreira <pedrocmoreira97@gmail.com>
2021-06-26 11:54:15 -0300

    Add appointment page and modal to choice a server

---
+++ src/screens/Guilds/styles.ts
@@ -0,0 +1,13 @@
+import { Inter_100Thin } from '@expo-google-fonts/inter';
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container:{
+         flex: Inter_100Thin,
+         alignItems: 'center',
+         paddingTop: 24,
+    },
+    guilds: {
+        width: '100%',
+    }
+});
