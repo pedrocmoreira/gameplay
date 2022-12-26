e33cbdca5a8f590e14320b1add6887f1ce49a465
Pedro Moreira <pedrocmoreira97@gmail.com>
2022-12-26 20:23:59 -0300

    chore: adicionando imagens dinâmicas dos servidores e refatorando componente

--- src/components/GuildIcon/index.tsx
+++ src/components/GuildIcon/index.tsx
@@ -1,16 +1,32 @@
 import React from "react";
-import { Image } from "react-native";
+import { Image, View } from "react-native";
 
 import { styles } from './styles';
 
-export function GuildIcon() {
-    const uri = 'https://image.flaticon.com/icons/png/512/906/906361.png';
+import DiscordSvg from '../../assets/discord.svg'
+
+const { CDN_IMAGE } = process.env;
+
+type Props = {
+    guildId: string;
+    iconId: string | null;
+}
+
+export function GuildIcon({guildId, iconId}: Props) {
+    const uri = `${CDN_IMAGE}/icons/${guildId}/${iconId}.png`;
 
     return (
-       <Image
-        source={{uri}}
-        style={styles.image}
-        resizeMode="cover"
-       />
+        <View style={styles.container}>
+        {
+            iconId ?
+            <Image
+                source={{uri}}
+                style={styles.image}
+                resizeMode="cover"
+            /> 
+            :
+            <DiscordSvg width={40} height={40}/>
+        }
+        </View>
     );
 }
--- src/screens/Guilds/index.tsx
+++ src/screens/Guilds/index.tsx
@@ -1,59 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, FlatList } from 'react-native';
 import { Guild, GuildProps } from "../../components/Guild";
 import { ListDivider } from "../../components/ListDivider";
 
-import discord from "../../assets/discord.png";
-
-
 import { styles } from './styles';
+import { Load } from "../../components/Load";
+import { api } from "../../services/api";
 
 type Props = {
     handleGuildSelect: (guild: GuildProps) => void;
 }
 
 export function Guilds({handleGuildSelect}: Props) {
-    const guilds = [
-        {
-            id: '1',
-            name: 'Lendrios',
-            icon: null,
-            owner: true,
-        },
-        {
-            id: '2',
-            name: 'Lendários',
-            icon: null,
-            owner: true,
-        },
-        {
-            id: '3',
-            name: 'Lendários',
-            icon: null,
-            owner: true,
-        },
-        {
-            id: '4',
-            name: 'Lendários',
-            icon: null,
-            owner: true,
-        },
-        {
-            id: '5',
-            name: 'Lendários',
-            icon: null,
-            owner: true,
-        },
-        {
-            id: '6',
-            name: 'Lendários',
-            icon: null,
-            owner: true,
-        }
-    ]
+    const [guilds, setGuilds] = useState<GuildProps[]>([]);
+    const [loading, setLoading] = useState(true);
 
+    async function fetchGuilds(){
+        const response = await api.get('/users/@me/guilds');
+        setGuilds(response.data);
+        setLoading(false);
+    }
+
+
+    useEffect(() => {
+        fetchGuilds();
+    }, [])
     return (
         <View style={styles.container}>
+            {
+                loading ? <Load/> :
             <FlatList
                 data={guilds}
                 keyExtractor={item => item.id}
@@ -69,6 +44,7 @@ export function Guilds({handleGuildSelect}: Props) {
                 ListHeaderComponent={() => <ListDivider isCentered/> }
                 style={styles.guilds}
             />
+            }
         </View>
     )
 }
